452255e64871567374cb67395776220a51f03ab5
Dnouv <sdevanshu90@yahoo.com>
2022-11-04 04:50:08 +0000

    add new content type

---
+++ cms/src/api/web3store/controllers/web3store.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  web3store controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::web3store.web3store');
---
+++ cms/src/api/web3store/routes/web3store.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * web3store router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::web3store.web3store');
